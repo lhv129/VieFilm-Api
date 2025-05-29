bbf0dfec9520f4db79ef87490467bbf093c110ab
DESKTOP-MQR0I53\vietlh <lhv2k4@gmail.com>
2025-05-29 17:36:44 +0700

    parseInt duration

--- src/models/movieModel.js
+++ src/models/movieModel.js
@@ -13,7 +13,7 @@ const MOVIE_COLLECTION_SCHEMA = Joi.object({
     directors: Joi.string().required(),
     actors: Joi.string().trim().allow('').optional(),
     genres: Joi.string().required().trim().strict(),
-    language: Joi.string().required().valid("Tiếng Việt","Tiếng Anh","Tiếng Trung"),
+    language: Joi.string().required().valid("Tiếng Việt", "Tiếng Anh", "Tiếng Trung"),
     duration: Joi.number().integer().required(),
     rating: Joi.number().required(),
     releaseDate: Joi.date().timestamp("javascript"),
@@ -40,6 +40,7 @@ const getAll = async (req, res, next) => {
 }
 
 const validateBeforeCreate = async (data) => {
+    data.duration = parseInt(data.duration, 10);
     return await MOVIE_COLLECTION_SCHEMA.validateAsync(data, {
         abortEarly: false
     });
